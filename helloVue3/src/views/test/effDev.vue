024bb6bd378dada618517164c6ce28e2d7c72c0e
tigeedev <970853117@qq.com>
2024-04-15 00:07:25 +0800

    学习vue3高效开发章节

---
+++ helloVue3/src/views/test/effDev.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        {{ msg }}
+        <domChild></domChild>
+    </div>
+</template>
+
+<script setup lang="ts">
+    // 子组件导入即可使用
+    import domChild from './dom.vue'
+
+    defineProps(['name','userInfo'])
+
+    const props = defineProps(['name','userInfo'])
+
+    // 对每个 prop 进行类型检查
+    defineProps({
+        name: String,
+        userInfo: Object
+    })
+
+    console.log(props.name);
+
+    // emits 的接收方式变化
+    // 获取 emits
+    const emit = defineEmits(['update-name'])
+    
+    // 调用 emit
+    emit('update-name', 'Robert')
+
+
+    const msg = 'Hello'
+
+    // 通过该 API 显式暴露的数据，才可以在父组件拿到
+    defineExpose({
+        msg
+    })
+
+    // 顶级 await 的支持
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+    const json = await res.json()
+
+</script>
+
+<style scoped>
+
+</style>
