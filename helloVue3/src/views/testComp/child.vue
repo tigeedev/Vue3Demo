71e57c708eace0f808828c0cf9ece6ad65778485
tigeedev <970853117@qq.com>
2024-04-14 23:12:07 +0800

    学习vue3组件之间的通信

---
+++ helloVue3/src/views/testComp/child.vue
@@ -0,0 +1,40 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        // 可选，并提供默认值
+        title: {
+            type: String,
+            required: false,
+            default: '标题'
+        },
+
+        // 默认可选，单类型
+        index: Number,
+        
+        // 添加自定义校验
+        userName: {
+            type: String,
+
+            // 在这里校验用户名必须至少 3 个字
+            validator: (v) => v.length >= 5
+        },
+
+        uid: [String, Number]
+    },
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ helloVue3/src/views/testComp/father.vue
@@ -0,0 +1,44 @@
+<template>
+    <child 
+        title="父组件传值给子组件"
+        :index="1"
+        :uid="userInfo.id"
+        :user-name="userInfo.name"
+    />
+
+    <!-- 用 v-model 传递数据 -->
+    <child 
+        v-model:index="userInfo.id"
+        v-model:username="userInfo.name"
+    />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import  child from './child.vue'
+
+interface Member {
+    id: number
+    name: string
+}
+
+export default defineComponent({
+    components: {
+        child
+    },
+    setup () {
+        const userInfo: Member = {
+            id: 1,
+            name: '张三'
+        }
+
+        return {
+            userInfo
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
